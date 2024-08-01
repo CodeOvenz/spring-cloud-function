44b495825015da7b7661ada1b2fce63c310f7c0c
MinChul-Son <smc5236@gmail.com>
2024-08-01 16:51:36 +0900

    ⚙️chore: aws lambda method apply

--- spring-cloud-function-samples/function-sample-aws/src/main/java/example/FunctionConfiguration.java
+++ spring-cloud-function-samples/function-sample-aws/src/main/java/example/FunctionConfiguration.java
@@ -1,9 +1,17 @@
 package example;
 
+import java.util.List;
 import java.util.function.Function;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 
 @SpringBootApplication
 public class FunctionConfiguration {
@@ -21,4 +29,56 @@ public class FunctionConfiguration {
 	public Function<String, String> uppercase() {
 		return value -> value.toUpperCase();
 	}
+
+	@Bean
+	public Function<String, String> dynamoSample() {
+		Region region = Region.AP_NORTHEAST_2;
+		DynamoDbClient ddb = DynamoDbClient.builder()
+			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accessKey", "secretKey")))
+										   .region(region)
+										   .build();
+		listAllTables(ddb);
+		ddb.close();
+		return value -> uppercase().apply(value) + " from DynamoDB";
+	}
+
+	public static void listAllTables(DynamoDbClient ddb) {
+		boolean moreTables = true;
+		String lastName = null;
+
+		while (moreTables) {
+			try {
+				ListTablesResponse response = null;
+				if (lastName == null) {
+					ListTablesRequest request = ListTablesRequest.builder().build();
+					response = ddb.listTables(request);
+				} else {
+					ListTablesRequest request = ListTablesRequest.builder()
+																 .exclusiveStartTableName(lastName).build();
+					response = ddb.listTables(request);
+				}
+
+				List<String> tableNames = response.tableNames();
+				if (tableNames.size() > 0) {
+					for (String curName : tableNames) {
+						System.out.format("* %s\n", curName);
+					}
+				} else {
+					System.out.println("No tables found!");
+					System.exit(0);
+				}
+
+				lastName = response.lastEvaluatedTableName();
+				if (lastName == null) {
+					moreTables = false;
+				}
+
+			} catch (DynamoDbException e) {
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+		}
+		System.out.println("\nDone!");
+	}
+
 }
